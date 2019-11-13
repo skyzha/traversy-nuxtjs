cd3219f044de72755989459aeef184c367f0b16a
skyzha <rezapermana.aqi@gmail.com>
2019-11-13 08:52:42 +0700

    traversy nuxt js complete'

---
+++ components/SearchJokes.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input v-model="text" type="text" placeholder="Search Jokes..." />
+    <input id type="submit" value="Search Jokes" name />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchJokes',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('search-text', this.text)
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ pages/about.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+    <p>description jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About The App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px dotted #ccc;
+}
+
+.header .title {
+  font-size: 3rem;
+  color: #526488;
+}
+
+.header ul {
+  display: flex;
+}
+
+.header a {
+  display: inline-block;
+  background: #333;
+  color: #fff;
+  padding: 0.3rem 1rem;
+  margin-right: 0.5rem;
+}
+</style>
